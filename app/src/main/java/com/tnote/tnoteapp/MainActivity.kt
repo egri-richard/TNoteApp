065187f269afe35c7da3495c6ec250d0157100d9
egri-richard <egri.dani.r@gmail.com>
2022-03-16 17:11:45 +0100

    loginIntent

--- app/src/main/java/com/tnote/tnoteapp/MainActivity.kt
+++ app/src/main/java/com/tnote/tnoteapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.tnote.tnoteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tnote.tnoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnToLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
