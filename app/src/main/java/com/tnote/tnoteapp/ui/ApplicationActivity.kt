69abd9b31a16cabe882722ab81027b08258ce8f3
egri-richard <egri.dani.r@gmail.com>
2022-03-22 12:27:53 +0100

    fragment design progress

--- app/src/main/java/com/tnote/tnoteapp/ui/ApplicationActivity.kt
+++ app/src/main/java/com/tnote/tnoteapp/ui/ApplicationActivity.kt
@@ -2,14 +2,18 @@ package com.tnote.tnoteapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.tnote.tnoteapp.databinding.ActivityApplicationBinding
 
 class ApplicationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityApplicationBinding
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityApplicationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/tnote/tnoteapp/ui/fragments/NotesListFragment.kt
@@ -0,0 +1,4 @@
+package com.tnote.tnoteapp.ui.fragments
+
+class NotesListFragment {
+}
