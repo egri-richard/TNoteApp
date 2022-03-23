11c0e16b6db145158747c1db0ee7b1c17a0edfc2
egri-richard <egri.dani.r@gmail.com>
2022-03-23 22:01:13 +0100

    NotesRvAdapter progress + note item xml imported

---
+++ app/src/main/java/com/tnote/tnoteapp/adapters/NotesAdapter.kt
@@ -0,0 +1,42 @@
+package com.tnote.tnoteapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.tnote.tnoteapp.models.Note
+
+class NotesAdapter: RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+    inner class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
