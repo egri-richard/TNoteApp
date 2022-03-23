b82aa06f081b1d711411ced6f81367e5f224623b
egri-richard <egri.dani.r@gmail.com>
2022-03-23 23:24:08 +0100

    Fragment viewbinding + response handlig + Viewmodel not working neeeds fixing

--- app/src/main/java/com/tnote/tnoteapp/logic/ApplicationViewModel.kt
+++ app/src/main/java/com/tnote/tnoteapp/logic/ApplicationViewModel.kt
@@ -1,7 +1,34 @@
 package com.tnote.tnoteapp.logic
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tnote.tnoteapp.api.ApiInstance
+import com.tnote.tnoteapp.models.Note
+import com.tnote.tnoteapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class ApplicationViewModel: ViewModel() {
+    val notesListFragmentState: MutableLiveData<Resource<List<Note>>> = MutableLiveData()
 
+    init {
+        //TODO: get user data and token into viewmodel somehow
+        getNotes()
+    }
+
+    fun getNotes(userId: Int, token: String) = viewModelScope.launch {
+        notesListFragmentState.postValue(Resource.Loading())
+        val response = ApiInstance.api.getNotes(userId, token)
+        notesListFragmentState.postValue(handleNotesResponse(response))
+    }
+
+    private fun handleNotesResponse(response: Response<List<Note>>) : Resource<List<Note>> {
+        if (response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message(), null)
+    }
 }
--- app/src/main/java/com/tnote/tnoteapp/ui/fragments/NotesListFragment.kt
+++ app/src/main/java/com/tnote/tnoteapp/ui/fragments/NotesListFragment.kt
@@ -1,19 +1,79 @@
 package com.tnote.tnoteapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tnote.tnoteapp.R
+import com.tnote.tnoteapp.adapters.NotesAdapter
+import com.tnote.tnoteapp.databinding.FragmentNoteslistBinding
 import com.tnote.tnoteapp.logic.ApplicationViewModel
 import com.tnote.tnoteapp.ui.ApplicationActivity
+import com.tnote.tnoteapp.util.Resource
 
 class NotesListFragment: Fragment(R.layout.fragment_noteslist) {
+    private var _binding: FragmentNoteslistBinding? = null
+    private val binding get() = _binding!!
+
     lateinit var viewModel: ApplicationViewModel
+    lateinit var notesAdapter: NotesAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNoteslistBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as ApplicationActivity).viewModel
 
+        viewModel.notesListFragmentState.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { notesListResponse ->
+                        notesAdapter.differ.submitList(notesListResponse)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let {
+                        Log.e("NotesFragment", "Error occured: $it")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
 
+    private fun setupRV() {
+        notesAdapter = NotesAdapter()
+        binding.rvNotes.apply {
+            adapter = NotesAdapter()
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar() {
+        binding.notesListProgressBar.visibility = View.GONE
+    }
+
+    private fun showProgressBar() {
+        binding.notesListProgressBar.visibility = View.VISIBLE
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
