96d63c6d1302adada8badf31be9f7ed790c90fcb
egri-richard <egri.dani.r@gmail.com>
2022-03-24 11:12:25 +0100

    SharedPreferences manager class

---
+++ app/src/main/java/com/tnote/tnoteapp/util/ApplicationViewModelFactory.kt
@@ -0,0 +1,12 @@
+package com.tnote.tnoteapp.util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ApplicationViewModelFactory: ViewModelProvider.Factory(
+
+) {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+    }
+}
---
+++ app/src/main/java/com/tnote/tnoteapp/util/SessionManager.kt
@@ -0,0 +1,35 @@
+package com.tnote.tnoteapp.util
+
+import android.content.Context
+import com.tnote.tnoteapp.R
+
+class SessionManager(context: Context) {
+    private val prefs = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
+
+    companion object {
+        const val USER_TOKEN = "user_token"
+        const val USER_ID = "user_id"
+        const val CURRENT_NOTE = "current_note"
+        const val CURRENT_TIMETABLE = "current_timetable"
+    }
+
+    fun clearPrefs() {
+        return prefs.all.clear()
+    }
+
+    fun saveCredentials(token: String, id: Int) {
+        val editor = prefs.edit()
+        editor.putString(USER_TOKEN, token)
+        editor.putInt(USER_ID, id)
+        editor.apply()
+    }
+
+    fun getUserId(): Int {
+        return prefs.getInt(USER_ID, 0)
+    }
+
+    fun getAuthToken(): String {
+        val token = prefs.getString(USER_TOKEN, null)
+        return "Bearer $token"
+    }
+}
