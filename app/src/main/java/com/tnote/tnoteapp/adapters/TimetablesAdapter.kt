12aedf6502c58a5b5094b6c36dc9b13064e423ec
egri-richard <egri.dani.r@gmail.com>
2022-03-25 12:23:05 +0100

    timetable adapter started + timetable listelement xml created

---
+++ app/src/main/java/com/tnote/tnoteapp/adapters/TimetablesAdapter.kt
@@ -0,0 +1,8 @@
+package com.tnote.tnoteapp.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+
+class TimetablesAdapter: RecyclerView.Adapter<TimetablesAdapter.TimetableViewHodler>() {
+
+
+}
