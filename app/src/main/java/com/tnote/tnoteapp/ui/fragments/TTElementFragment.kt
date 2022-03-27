5ec3d647a6b0c1d7a24006904c4517b00171cecf
egri-richard <egri.dani.r@gmail.com>
2022-03-27 22:22:38 +0200

    ttelement spinner

--- app/src/main/java/com/tnote/tnoteapp/ui/fragments/TTElementFragment.kt
+++ app/src/main/java/com/tnote/tnoteapp/ui/fragments/TTElementFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
+import com.tnote.tnoteapp.R
 import com.tnote.tnoteapp.databinding.FragmentTtelementBinding
 import com.tnote.tnoteapp.logic.ApplicationViewModel
 import com.tnote.tnoteapp.ui.ApplicationActivity
+import com.tnote.tnoteapp.util.Resource
 import com.tnote.tnoteapp.util.SessionManager
 
 class TTElementFragment : Fragment() {
@@ -20,6 +25,7 @@ class TTElementFragment : Fragment() {
 
     val args: TTElementFragmentArgs by navArgs()
 
+    lateinit var spAdapter: ArrayAdapter<String>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,15 +39,52 @@ class TTElementFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as ApplicationActivity).viewModel
         sessionManager = SessionManager(requireContext())
-
+        setupSpAdapter()
 
         val selectedElementId = args.ttElementId
 
+        viewModel.getSelectedTTELment(
+            selectedElementId,
+            sessionManager.getAuthToken()
+        )
 
+
+        viewModel.ttElementFragmentState.observe(viewLifecycleOwner) {
+            when(it) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    //TODO: fragment design + fill with data here
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    Snackbar.make(
+                        binding.root,
+                        "Unexpected Error",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+                is Resource.Loading -> showProgressBar()
+            }
+        }
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun hideProgressBar() {
+        binding.ttElementFragmentProgressBar.visibility = View.GONE
+    }
+
+    private fun showProgressBar() {
+        binding.ttElementFragmentProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupSpAdapter() {
+        spAdapter = ArrayAdapter<String>(
+            requireContext(),
+            R.layout.spitem_day,
+            resources.getStringArray(R.array.days_array))
     }
 }
