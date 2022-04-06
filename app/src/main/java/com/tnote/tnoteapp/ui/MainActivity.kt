f382d7ad2e273e690b06ce04362947d431a50f47
egri-richard <egri.dani.r@gmail.com>
2022-04-06 13:03:49 +0200

    Mainactivity login check is now a function and MainActivity viewmodel deleted because unnecesary

--- app/src/main/java/com/tnote/tnoteapp/ui/MainActivity.kt
+++ app/src/main/java/com/tnote/tnoteapp/ui/MainActivity.kt
@@ -5,26 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.tnote.tnoteapp.databinding.ActivityMainBinding
-import com.tnote.tnoteapp.logic.MainActivityViewModel
 import com.tnote.tnoteapp.util.SessionManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainActivityViewModel
     private lateinit var sessionManager: SessionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel = MainActivityViewModel()
         sessionManager = SessionManager(applicationContext)
 
         Log.e("MainActivty", "SessionManager data: id: ${sessionManager.getUserId()} token: ${sessionManager.getAuthToken()}", )
-        if (sessionManager.getUserId() > 0) {
-            startActivity(Intent(this, ApplicationActivity::class.java))
-            finish()
-        }
+        isLoggedIn(sessionManager.getUserId())
+
 
         binding.btnToLogin.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
@@ -33,6 +28,13 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnToRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun isLoggedIn(userId: Int) {
+        if (userId > 0) {
+            startActivity(Intent(this, ApplicationActivity::class.java))
             finish()
         }
     }
